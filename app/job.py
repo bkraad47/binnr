821bb7a83cafadf0d3a9b3468f29b198ec25bbd4
bkraad47 <bulkguy47@gmail.com>
2024-07-16 06:20:57 +1000

    Add LLM Classification

--- app/job.py
+++ app/job.py
@@ -1,6 +1,7 @@
 from celery import Celery
 from Snowflake.data_util import load_data_to_snowflake, create_classification_tables
 from Snowflake.gbm_classification import train_model, generate_predictions
+from Snowflake.llm_classification import train_llm, predict_llm
 import os
 
 celery = Celery(__name__, broker=os.getenv('CELERY_BROKER_URL'), backend=os.getenv('CELERY_RESULT_BACKEND'))
@@ -20,3 +21,11 @@ def run_train_model(database, schema, role, warehouse, training_table):
 @celery.task
 def run_generate_predictions(database, schema, role, warehouse, model_name, predict_table):
     return generate_predictions(database, schema, role, warehouse, model_name, predict_table)
+
+@celery.task
+def run_train_llm(training_table, validation_table, target_column, database, schema, role, warehouse, model_type):
+    return train_llm(training_table, validation_table, target_column, database, schema, role, warehouse, model_type)
+
+@celery.task
+def run_predict_llm(predict_table, model_name, database, schema, role, warehouse):
+    return predict_llm(predict_table, model_name, database, schema, role, warehouse)
